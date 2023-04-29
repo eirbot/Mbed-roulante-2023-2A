1c55101797e6423f990fe4cc3223d3c684f00094
leov66 <leo.v66.lov@gmail.com>
2023-04-29 19:31:51 +0200

    fill based fonctions

--- src/motor_brushless_eirbot.cpp
+++ src/motor_brushless_eirbot.cpp
@@ -8,27 +8,146 @@
 
 #include "motor_brushless_eirbot.h"
 
+#include "tim.h"
+
+void TIMER1_init() {
+    MX_TIM1_Init();
+
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+    HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+    HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_2);
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_3);
+    HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_3);
+}
+
+void TIMER8_init() {
+    MX_TIM8_Init();
+
+    HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_1);
+    HAL_TIMEx_PWMN_Start(&htim8, TIM_CHANNEL_1);
+    HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_2);
+    HAL_TIMEx_PWMN_Start(&htim8, TIM_CHANNEL_2);
+    HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_3);
+    HAL_TIMEx_PWMN_Start(&htim8, TIM_CHANNEL_3);
+}
+
+
+
+
 namespace sixtron {
 
     void MotorBrushlessEirbot::initHardware() {
         // Init Motor PWM
+        if (_positionMotor == Left) {
+            // Configuration des Hall Sensors
+            _pinHall_1 = PB_2;
+            _pinHall_2 = PB_5;
+            _pinHall_3 = PB_6;
+            _tim = TIM1;
+            TIMER1_init();
+        }
+        else if (_positionMotor == Right) {
+            _pinHall_1 = PB_7;
+            _pinHall_2 = PB_8;
+            _pinHall_3 = PB_9;
+            _tim = TIM8;
+            TIMER8_init();
+        }
 
+        _hallWord_previous = 0b000;
+        halfBridgeApply(halfBridgeZEROS);
+
+        HALL_1 = new InterruptIn(_pinHall_1, PullNone);
+        HALL_2 = new InterruptIn(_pinHall_2, PullNone);
+        HALL_3 = new InterruptIn(_pinHall_3, PullNone);
+        HALL_1->rise(callback(this, &MotorBrushlessEirbot::hallInterrupt));
+        HALL_2->rise(callback(this, &MotorBrushlessEirbot::hallInterrupt));
+        HALL_3->rise(callback(this, &MotorBrushlessEirbot::hallInterrupt));
     }
 
     float MotorBrushlessEirbot::getSensorSpeed() {
-
-
+        // réducteur 14
+        // nb tick par tour non réduit 48
+        return 14*48;
     }
 
     void MotorBrushlessEirbot::setPWM(float pwm) {
         // update hardware motor PWM
 
+        if (pwm >= 0) { _sens = clockwise; }
+        else { _sens = antiClockwise; }
+
+        // Conversion vers du uint8_t
+        uint8_t dutyCycle_int = (uint8_t) (abs(pwm) * 100);
+
+        // Saturation
+        if (dutyCycle_int >= 90) { dutyCycle_int = 90; }
+
+        _tim->CCR1 = dutyCycle_int;
+        _tim->CCR2 = dutyCycle_int;
+        _tim->CCR3 = dutyCycle_int;
     }
 
-	int32_t MotorBrushlessEirbot::getHALLticks() {
-
-
-
-		return 0;
+    int32_t MotorBrushlessEirbot::getHALLticks() {
+        return _ticks;
 	}
+
+    void MotorBrushlessEirbot::hallInterrupt() {
+        // Lecture Hall sensors
+        volatile uint8_t hallWord = (HALL_1->read() << 2) | (HALL_2->read() << 1) | HALL_3->read();
+
+        // Affectation de la séquence d'après
+        halfBridge_t halfBridge = {false, false, false, false, false, false};
+        if (_sens == clockwise) {
+            halfBridge = clockwiseSequence[hallWord - 1];
+            _ticks++; // Indentation des ticks
+        } else if (_sens == antiClockwise) {
+            halfBridge = antiClockwiseSequence[hallWord - 1];
+            _ticks--; // Indentation des ticks
+        }
+
+        halfBridgeApply(halfBridge);
+
+        _hallWord_previous = hallWord; // Retenir la valeur précédente
+    }
+
+    void MotorBrushlessEirbot::halfBridgeApply(halfBridge_t halfBridgeConfig) {
+        // Mise en œuvre des commandes
+        if (halfBridgeConfig.aH) {                //CC1E=1
+            _tim->CCER |= TIM_CCER_CC1E;
+        } else {                                  //CC1E=0
+            _tim->CCER &= ~(TIM_CCER_CC1E);
+        }
+
+        if (halfBridgeConfig.aL) {                //CC1NE=1
+            _tim->CCER |= TIM_CCER_CC1NE;
+        } else {                                  //CC1NE=0
+            _tim->CCER &= ~(TIM_CCER_CC1NE);
+        }
+
+        if (halfBridgeConfig.bH) {
+            _tim->CCER |= TIM_CCER_CC2E;
+        } else {
+            _tim->CCER &= ~(TIM_CCER_CC2E);
+        }
+
+        if (halfBridgeConfig.bL) {
+            _tim->CCER |= TIM_CCER_CC2NE;
+        } else {
+            _tim->CCER &= ~(TIM_CCER_CC2NE);
+        }
+
+        if (halfBridgeConfig.cH) {
+            _tim->CCER |= TIM_CCER_CC3E;
+        } else {
+            _tim->CCER &= ~(TIM_CCER_CC3E);
+        }
+
+        if (halfBridgeConfig.cL) {
+            _tim->CCER |= TIM_CCER_CC3NE;
+        } else {
+            _tim->CCER &= ~(TIM_CCER_CC3NE);
+        }
+    }
 } // namespace sixtron
--- src/motor_brushless_eirbot.h
+++ src/motor_brushless_eirbot.h
@@ -14,15 +14,46 @@
 
 namespace sixtron {
 
+    enum position {
+        Left, Right
+    };
+    enum rotationSens_t {
+        clockwise, antiClockwise
+    };
+    typedef struct {
+        bool aH;
+        bool aL;
+        bool bH;
+        bool bL;
+        bool cH;
+        bool cL;
+    } halfBridge_t;
+    const halfBridge_t clockwiseSequence[6] = { //  101 -> 001 -> 011 -> 010 -> 110 -> 100
+            {false, true,  false, false, true,  false}, // 0b001
+            {true,  false, false, true,  false, false}, // 0b010
+            {true,  false, false, false, false, true},  // 0b011
+            {false, true,  false, false, true,  false}, // 0b100
+            {false, true,  true,  false, false, false}, // 0b101
+            {false, false, false, true,  true,  false}, // 0b110
+    };
+    const halfBridge_t antiClockwiseSequence[6] = {
+            {false, false, false, true,  true,  false}, // 0b001
+            {false, true,  true,  false, false, false}, // 0b010
+            {false, true,  false, false, true,  false}, // 0b011
+            {true,  false, false, false, false, true},  // 0b100
+            {true,  false, false, true,  false, false}, // 0b101
+            {false, false, true,  false, false, true},  // 0b110
+    };
+
 #define DEFAULT_MOTOR_MAX_PWM 1.0f // max PWM with mbed is 1.0f
 
     class MotorBrushlessEirbot: public MotorDC {
 
     public:
-		MotorBrushlessEirbot(float rate_dt,
+		MotorBrushlessEirbot(float rate_dt, position motor_position,
                 PID_params motor_pid,
                 float max_pwm = DEFAULT_MOTOR_MAX_PWM):
-                MotorDC(rate_dt, motor_pid, max_pwm) {};
+                MotorDC(rate_dt, motor_pid, max_pwm), _positionMotor(motor_position) {};
 
     private:
         void initHardware() override;
@@ -31,9 +62,21 @@ namespace sixtron {
 		int32_t getHALLticks();
 
     private:
-
+        TIM_TypeDef *_tim;
+        position _positionMotor;
+        rotationSens_t _sens;
+        InterruptIn *HALL_1;
+        InterruptIn *HALL_2;
+        InterruptIn *HALL_3;
+        void hallInterrupt();
+        void halfBridgeApply(halfBridge_t halfBridgeConfig);
+        const halfBridge_t halfBridgeZEROS = {0, 0, 0, 0, 0, 0};
+        volatile uint8_t _hallWord_previous;
+        volatile int32_t _ticks;
+        PinName _pinHall_1;
+        PinName _pinHall_2;
+        PinName _pinHall_3;
     };
 
-} // namespace sixtron
-
+}; // namespace sixtron
 #endif // CATIE_SIXTRON_MOTOR_BRUSHLESS_EIRBOT_H
